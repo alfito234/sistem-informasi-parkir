19998e70a7af478f54b02b16b5ce7628fcae4e22
Densa Tanzilda Widyawantara Dzahy <101276402+densatanz@users.noreply.github.com>
2022-03-19 17:19:16 +0700

    Add files via upload

---
+++ create.php
@@ -0,0 +1,27 @@
+<?php
+
+include '../connect.php';
+
+$idKendaraan = $_POST['idInfo'];
+$nopol = $_POST['parkirSlot'];
+$jenis = $_POST['jumlahKendaraan'];
+
+$query = "INSERT INTO kendaraan(idInfo, parkirSlot, jumlahKendaraan) 
+          VALUES('$idInfo','$parkirSlot','$jumlahKendaraan')";
+
+$result = mysqli_query($connect, $query);
+
+$num = mysqli_affected_rows($connect);
+
+if($num > 0)
+{
+  echo "Berhasil tambah data <br>";
+}
+else
+{
+  echo "Gagal tambah data <br>";
+}
+
+echo "<a href='read.php'>Lihat data</a>";
+
+?>
